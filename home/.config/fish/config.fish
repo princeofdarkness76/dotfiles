50f5494fe3636d70ce41ba9dbc36ce567b561d90
Stevie James <djsteviejames@hotmail.com>
2015-12-11 22:55:49 -0800

    config.nice
    added config.nice

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,11 @@
+# /Users/stevenjames/.phpbrew/phpbrew.fish
+
+# /bin/bash
+
+# /alias
+
+# PATH="/usr/local/sbin:$PATH"
+
+# export PATH="(brew --prefix homebrew/php/php55)/bin:PATH"
+
+# PATH="/usr/local/bin:$PATH" --without-homebrew-php
